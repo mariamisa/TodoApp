f7773bed1ddc150d5df699ca7a005b9a9e547149
mariamisa <maryaam1998@hotmail.com>
2020-05-01 21:16:58 +0300

    add to todo relates #3

--- client/src/componant/Todo.js
+++ client/src/componant/Todo.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 
 export default function Todo(){
+    const [todo,setTodo]=useState('')
+    const [todoList,setTodoList]=useState([])
+
+    const handelInputChange=({target:{value}})=>{
+        setTodo(value)
+    }
+    const addTodoToList=()=>{
+        setTodoList([...todoList,todo]);
+        setTodo('')
+    }
     return (
-        <div>todo app</div>
+        <div>
+            <input name="todo" value={todo} onChange={handelInputChange}/>
+            <button onClick={addTodoToList}>add todo</button>
+            <ul>{todoList.map((todo,index)=><li key={index}>{todo}</li>)}</ul>
+        </div>
     )
 }
